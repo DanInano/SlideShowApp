78b209cbc59bc0b94d363c63967c1ebb44736f9f
dan.inano <dan.inano@icloud.com>
2020-08-18 17:33:09 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Dan Inano on 2020/08/18.
+//  Copyright © 2020 Dan Inano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
