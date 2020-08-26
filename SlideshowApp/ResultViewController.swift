c5c4686563eab013b1944c03d36eef5cf1e5d546
dan.inano <dan.inano@icloud.com>
2020-08-26 23:23:14 +0900

    timer completed

---
+++ SlideshowApp/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  SlideshowApp
+//
+//  Created by Dan Inano on 2020/08/20.
+//  Copyright © 2020 Dan Inano. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,91 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
+    var dispImageNo = 0
+    
+    func displayImage(){
+        let imageNameArray = ["1.jpg" ,"2.jpg","3.jpg",]
+
+        let name = imageNameArray[dispImageNo]
+        
+        let image = UIImage (named: name)
+        
+        
+        imageView.image = image
+    }
+    
+    func slideShow(){
+        if timer_sec%2 == 0{
+            if dispImageNo != 2{
+                dispImageNo += 1
+            }
+            else {
+                dispImageNo = 0
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: "1.jpg")
     }
 
+//進むボタン
+    @IBAction func next(_ sender: Any) {
+        if self.timer == nil{
+            if dispImageNo == 2{
+                dispImageNo = 0
+            }
+            else{
+                dispImageNo += 1
+            }
+            displayImage()
+        }
+    }
+//    戻るボタン
+    @IBAction func back(_ sender: Any) {
+        if self.timer == nil{
+            if dispImageNo == 0{
+                dispImageNo = 2
+            }
+            else{
+                dispImageNo -= 1
+            }
+            displayImage()
+        }
+    }
+    
+//    スライドショー用タイマー定義
+    var timer: Timer!
+    var timer_sec: Int = 0
+    @objc func updateTimer(_ timer: Timer){
+        self.timer_sec += 1
+        
+        slideShow()
+        
+        displayImage()
+    }
+    
+    
+    @IBOutlet weak var timerlabel: UILabel!
+    
+    //    再生/停止ボタン
+    @IBAction func startstop(_ sender: Any) {
+        if self.timer == nil{   //タイマー始動
+            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:#selector (updateTimer(_:)), userInfo: nil, repeats: true)
+            
 
+        }
+        else {                 //タイマー停止
+            self.timer_sec = 0
+            self.timer.invalidate()
+            self.timer = nil
+        }
+        
+
+    }
 }
-
